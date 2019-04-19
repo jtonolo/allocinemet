5655c6157c7c9228df7337c9d5fbea36d5c7fcac
jtonolo <jules.tonolo@gmail.com>
2019-04-19 15:48:59 +0200

    amelioration et ajout code yoni

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Exemple</title>
+    <meta charset="UTF-8">
+    <link href="index.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="parallax-window" data-parallax="scroll" data-image-src="./img/pop.jpeg"></div>
+    <div class="container-titre mx-auto my-5 text-center">
+        <h1>ALLOCINEMET</h1>
+        <hr class="col-4" style="background-color: red;">
+    </div>
+    <div class="container-fluid col-12 mx-auto my-5">
+        <div class="image float-left col-4">
+            <img src="https://i.ytimg.com/vi/GXM838nBLuw/maxresdefault.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+        <div class="container-fluid mx-auto my-5">
+            <h2 class="text-center container-fluid mx-auto">Lorem Ipsum</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga error reiciendis repellat, aut quos
+                culpa
+                distinctio tempora a laudantium voluptatem eligendi possimus nesciunt quas maiores, asperiores
+                beatae
+                esse? Nulla, totam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, nesciunt sapiente. Magnam dicta
+                eligendi
+                repellendus repellat dolore saepe rem totam. Possimus quam illo aliquam repudiandae ipsam nam ad, et
+                quis?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, nesciunt sapiente. Magnam dicta
+                eligendi
+                repellendus repellat dolore saepe rem totam. Possimus quam illo aliquam repudiandae ipsam nam ad, et
+                quis?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, nesciunt sapiente. Magnam dicta
+                eligendi
+                repellendus repellat dolore saepe rem totam. Possimus quam illo aliquam repudiandae ipsam nam ad, et
+                quis?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita minus nisi sed cum, molestiae
+                voluptates est voluptatibus architecto iusto molestias esse sapiente hic nulla quasi vel aperiam a
+                ex tempora!</p>
+        </div>
+    </div>
+    <div class="parallax-window" data-parallax="scroll" data-image-src="./img/ab1.jpg"></div>
+    <script src="js/app.js"></script>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="js/parallax.min.js"></script>
+</body>
+
+</html>
